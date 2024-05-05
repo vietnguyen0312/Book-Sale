947c4092baf21832854cb3a87640a46b6788efa7
Hoàng Việt <132984692+vietnguyen0312@users.noreply.github.com>
2024-05-05 19:17:57 +0700

    Delete BookSale-Project-b155e0a64348763f4f3be9320b673cdf797db8f4 directory

--- BookSale-Project-b155e0a64348763f4f3be9320b673cdf797db8f4/BookSaleProject/src/main/java/com/example/BookSaleProject/Model/BaseConnection.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookSaleProject.Model;
-
-public class BaseConnection {
-    public static String url= "jdbc:mysql://localhost:3306/booksale";
-    public static String username = "root";
-    public static String password="diquezrivenlol123";
-    public static String nameClass="com.mysql.cj.jdbc.Driver";
-}
--- BookSale-Project-b155e0a64348763f4f3be9320b673cdf797db8f4/BookSaleProject/src/main/java/com/example/BookSaleProject/Model/Service/IRateService.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookSaleProject.Model.Service;
-
-import com.example.BookSaleProject.Model.Entity.Book;
-
-public interface IRateService {
-    public double getScoreByIdBook(Book book);
-}
