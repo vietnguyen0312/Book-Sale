10c69bc60f620eb341bd24f9fdf8454d14b1b181
Hoàng Việt <vietnguyen0312@gmail.com>
2024-05-06 16:36:34 +0700

    AIP gg

--- BookSaleProject/src/main/java/com/example/BookSaleProject/Controller/BookController.java
+++ BookSaleProject/src/main/java/com/example/BookSaleProject/Controller/BookController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -75,7 +74,7 @@ public class BookController {
         model.addAttribute("bookTypeList", bookTypeList);
         model.addAttribute("newBookList", bookRate);
         model.addAttribute("favouriteBookList", topRatedBooks);
-        return "index";
+        return "test";
     }
 
     @GetMapping(value = { "/getBookList/{pageNum}" })
@@ -210,8 +209,8 @@ public class BookController {
     }
 
     @PostMapping(value = { "/updateBook" })
-    public String updateBook(Model model, @ModelAttribute("bookFinded") Book book) {
-        bookService.update(book);
+    public String updateBook(Model model, @RequestParam("bookFinded") String id) {
+        
         return "";
     }
 
